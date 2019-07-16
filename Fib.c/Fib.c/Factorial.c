64b0db6f054ee5c8d58bc797472bfb357d3a1e7e
leiziying <3032483701@qq.com>
2019-07-16 08:55:04 +0800

    提交代码

---
+++ Fib.c/Fib.c/Factorial.c
@@ -0,0 +1,29 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<stdlib.h>
+ int  BigitSum(int n)
+ {
+	 int i = 0;
+	 for ()
+	 
+	 for (i = 1; i <= n; i++)
+	 {
+		 n /= 10;
+		 i++;
+	 }
+
+
+}
+
+
+
+
+int main()
+{
+	int n = 1729;
+	printf("%d",BigitSum(n))
+
+
+	system("pause");
+	return 0;
+}
